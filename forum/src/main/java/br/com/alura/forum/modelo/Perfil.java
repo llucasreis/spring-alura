43cfcb6dfca2e8b3a6a1409b10d28584d529d878
Lucas Pereira Reis <lucas.reis@grupoicts.com.br>
2020-02-28 17:31:01 -0400

    feat: create security and authentication services

---
+++ forum/src/main/java/br/com/alura/forum/modelo/Perfil.java
@@ -0,0 +1,54 @@
+package br.com.alura.forum.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.security.core.GrantedAuthority;
+
+@Entity
+public class Perfil implements GrantedAuthority {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    /**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String getAuthority() {
+        return nome;
+    }
+
+}
--- forum/src/main/resources/data.sql
+++ forum/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$AIdTcwdGUt.49tn4uEDzLuK0Aepg6nYjeJ50hClIx8dm3p4oB1f/6');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
